9318c5f924b863b900a07965fa389070464a931f
jeffersondrs <jeffersonduarte@id.uff.br>
2023-03-02 22:20:09 -0300

    home working local

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,28 @@
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useQuery } from "react-query";
-import { useState } from "react";
 import Footer from "../components/footer/Footer";
+import { useState } from "react";
 
 const url = import.meta.env.VITE_API_URL;
-const isLoggedIn: boolean = false;
 
 export default function Home(): JSX.Element {
-  const [isLogged, setIsLogged] = useState(isLoggedIn);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const { isLoading, error, data } = useQuery(
-    "barbers",
-    async () =>
-      await axios
-        .get(`${url}master/staffs`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        })
-        .then((res) => {
-          res.data.data.staff;
-          setIsLogged(true);
-        })
-        .catch((err) => console.log(err))
-        .finally(() => console.log("logged"))
+  const { isLoading, error, data } = useQuery("barbers", () =>
+    axios
+      .get(`${url}/master/staffs`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => res.data.data.staff)
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoggedIn(true))
   );
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <div>Loaading...</div>;
+  if (error) return <>Faça login para acessar essa página</>;
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -35,29 +30,45 @@ export default function Home(): JSX.Element {
 
   return (
     <>
-      {isLogged ? (
-        <div className="w-full h-screen flex flex-col justify-center items-center">
-          <h1 className="text-4xl">Welcome to Barber</h1>
-          {
-            // @ts-ignore
-            data.map((barber: any) => {
+      {isLoggedIn ? (
+        <div className="w-full h-screen flex flex-col justify-between items-center">
+          <nav className="w-full h-20 border-b-2 border-solid border-violet-700 bg-slate-300 shadow-lg flex justify-between items-center p-5">
+            <h1 className="text-4xl">Barber</h1>
+            <Link to="/" onClick={handleLogout}>
+              <button className="flex-shrink-0 bg-teal-500 transition-all hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded w-36 uppercase font-bold">
+                Logout
+              </button>
+            </Link>
+          </nav>
+          <h1 className="text-4xl m-10">Bem vindo ao admin da Barbearia</h1>
+          <p className="text-lg">
+            Aqui teremos rotas para serem ilustradas, já que todos os dados do
+            servidor serão administrados por este dashboard.
+          </p>
+          <p>
+            Em breve irei colocar os endpoints também para facilitar nossa vida.
+          </p>
+          <p>Por enquanto, vamos ver os dados do servidor:</p>
+          <div className="h-full w-full">
+            {data.map((barber: any) => {
               return (
-                <div key={barber.id}>
-                  <h1>{barber.name}</h1>
-                  <p>{barber.email}</p>
-                  <p>{barber.photo}</p>
-                  <p>{barber.role}</p>
-                  <p>{barber.createdAt}</p>
-                  <p>{barber._id}</p>
+                <div
+                  className="h-56 w-80 flex flex-col  justify-center items-start p-5 rounded-xl shadow-xl"
+                  key={barber.id}
+                >
+                  <h1 className="text-xl font-bold text-center w-full">
+                    {barber.name}
+                  </h1>
+                  <p className="text-lg">{barber.email}</p>
+                  <p className="text-lg">{barber.photo}</p>
+                  <p className="text-lg">{barber.role}</p>
+                  <p className="text-lg">{barber.createdAt}</p>
+                  <p className="text-lg">{barber._id}</p>
                 </div>
               );
-            })
-          }
-          <Link to="/login" onClick={handleLogout}>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-              Logout
-            </button>
-          </Link>
+            })}
+          </div>
+          <Footer />
         </div>
       ) : (
         <div className="w-full h-screen flex flex-col justify-center items-center">
@@ -69,7 +80,7 @@ export default function Home(): JSX.Element {
               Faça seu login para continuar
             </p>
             <Link to="/">
-              <button className="bg-sky-500 w-40 h-12 text-xl uppercase hover:bg-sky-700 text-white font-bold py-2 px-4 rounded transition-all ease-linear">
+              <button className="flex-shrink-0 bg-teal-500 transition-all hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded w-36 uppercase font-bold">
                 Login
               </button>
             </Link>
